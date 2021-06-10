1aedb2b3ebd706218cbbef42dc5e1887f7be15ba
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2021-06-10 12:01:15 +1200

    Add start menu and restart menu

--- Prod 323 Assignment 2/Assets/Scripts/GameManager.cs
+++ Prod 323 Assignment 2/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,17 +12,30 @@ public class GameManager : MonoBehaviour
     public GameObject soldier;
     public Transform bornPoint;
     public ArmyManager _am;
+    public GameObject startUI;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Time.timeScale = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        startUI.SetActive(false);
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 
 
